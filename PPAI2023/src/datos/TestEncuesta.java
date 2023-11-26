82f47afc13621fcb7dcd509521f0136d6a243c0c
Gonzalo Baldassi <gonzalobaldassi02@gmail.com>
2023-11-26 17:51:18 -0300

    Conexion con la DB para las llamadas. Faltan las encuestas

--- PPAI2023/src/datos/TestEncuesta.java
+++ PPAI2023/src/datos/TestEncuesta.java
@@ -32,205 +32,74 @@ public class TestEncuesta {
             Connection conectar = DriverManager.getConnection(BD, usuario, password);
             PreparedStatement queryLlamadas;
             PreparedStatement queryRespuestas;
-            PreparedStatement queryRespuestasPosibles;
+            
 
             ResultSet resultadoLlamadas;
             ResultSet resultadoRespuestas;
-            ResultSet resultadoRespuestasPosibles;
+            
 
             
 
-            queryLlamadas = conectar.prepareCall("SELECT * FROM Llamada INNER JOIN Estado ON Llamada.estado = Estado.id INNER JOIN Cliente ON Llamada.cliente = Cliente.id");
-            queryRespuestas = conectar.prepareCall("SELECT * FROM RespuestaDeCliente INNER JOIN RespuestaPosible ON RespuestaDeCliente.respuestaPosible = RespuestaPosible.id INNER JOIN Llamada ON RespuestaDeCliente.llamada = Llamada.numeroLlamada");
-            queryRespuestasPosibles = conectar.prepareCall("SELECT * FROM RespuestaPosible");
+            queryLlamadas = conectar.prepareCall("SELECT * FROM Llamada INNER JOIN Estado ON Llamada.estado = Estado.id INNER JOIN Cliente ON Llamada.cliente = Cliente.id", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            queryRespuestas = conectar.prepareCall("SELECT * FROM RespuestaDeCliente INNER JOIN RespuestaPosible ON RespuestaDeCliente.respuestaPosible = RespuestaPosible.id INNER JOIN Llamada ON RespuestaDeCliente.llamada = Llamada.numeroLlamada", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            
 
             resultadoLlamadas = queryLlamadas.executeQuery();
             resultadoRespuestas = queryRespuestas.executeQuery();
-            resultadoRespuestasPosibles = queryRespuestasPosibles.executeQuery();
-
+            
+            
             while (resultadoLlamadas.next()) {
-
+                
                 int numeroLlamada = resultadoLlamadas.getInt(1);
                 ArrayList<RespuestaDeCliente> respuestas = new ArrayList<RespuestaDeCliente>();
-
+               
                 while (resultadoRespuestas.next()) {
-                    /*int numeroRespuestaPosible = resultadoRespuestas.getInt(3);
-                    if (resultadoRespuestas.getInt(4) == numeroLlamada) {
-                        while (resultadoRespuestasPosibles.next()) {
-                            if (numeroRespuestaPosible == resultadoRespuestasPosibles.getInt(1)) {
-                                RespuestaPosible respuestaPosible = new RespuestaPosible(resultadoRespuestasPosibles.getString(2), resultadoRespuestasPosibles.getInt(3));
-                                RespuestaDeCliente respuestaDeCliente = new RespuestaDeCliente(respuestaPosible, resultadoRespuestas.getDate(2));
-                                respuestas.add(respuestaDeCliente);
-                            }
-                        }
-
-                    }*/
-                    if (resultadoRespuestas.getInt(4) == numeroLlamada) {
-                        Date inputDate = resultadoRespuestas.getDate(2);
+                    
+                    if (resultadoRespuestas.getInt(5) == numeroLlamada) {
+                        Date inputDate = resultadoRespuestas.getDate(3);
                         String outputDateFormat = "MM/dd/yyyy";
-
                         SimpleDateFormat outputFormat = new SimpleDateFormat(outputDateFormat);
-
-                        String outputDateString = outputFormat.format(inputDate);
-                        //System.out.println("Fecha de entrada: " + inputDate);
-                        //System.out.println("Fecha de salida: " + outputDateString);
-                        
-                        
+                        String outputDateString = outputFormat.format(inputDate);  
                         Date f = new Date(outputDateString);
-                        RespuestaPosible respuestaPosible = new RespuestaPosible(resultadoRespuestas.getString(6), resultadoRespuestas.getInt(7));
+                        RespuestaPosible respuestaPosible = new RespuestaPosible(resultadoRespuestas.getString(7), resultadoRespuestas.getInt(8));
                         RespuestaDeCliente respuestaDeCliente = new RespuestaDeCliente(respuestaPosible, f);
                         respuestas.add(respuestaDeCliente);
                     }
 
                 }
-
+                resultadoRespuestas.beforeFirst();
+                
                 Cliente cliente = new Cliente(resultadoLlamadas.getString(12), resultadoLlamadas.getInt(13), resultadoLlamadas.getString(14));
                 Estado estado = new Estado(resultadoLlamadas.getString(10));
-
-                Llamada llamada = new Llamada(resultadoLlamadas.getInt(1), cliente, estado, respuestas, resultadoLlamadas.getString(2), resultadoLlamadas.getInt(4), resultadoLlamadas.getBoolean(5));
-                llamadas.add(llamada);
-                System.out.println(llamada);
+                if (!respuestas.isEmpty()){
+                    Llamada llamada = new Llamada(resultadoLlamadas.getInt(1), cliente, estado, respuestas, resultadoLlamadas.getString(2), resultadoLlamadas.getInt(4), resultadoLlamadas.getBoolean(5));
+                    llamadas.add(llamada);
+            
+                }else{
+                    Llamada llamada = new Llamada(resultadoLlamadas.getInt(1), cliente, estado, null, resultadoLlamadas.getString(2), resultadoLlamadas.getInt(4), resultadoLlamadas.getBoolean(5));
+                    
+                    llamadas.add(llamada);
+                }
+                
+                
+                
             }
+            
+            
+            queryLlamadas.close();
+            queryRespuestas.close();
+            conectar.close();
 
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
             System.out.println("Conexion NO realizada");
         }
 
-        //----------------------------------------------------------------------------------
-        //------------------------------ OBJETOS -------------------------------------------
-        //Creacion de objetos necesarios para el funcionamiento del CU
-        ArrayList<RespuestaPosible> vectorRespuestasCalificacion = new ArrayList<RespuestaPosible>();
-        ArrayList<RespuestaPosible> vectorRespuestasSiNo = new ArrayList<RespuestaPosible>();
 
-        //Creacion respuestas de clientes y agregacion al vector correspondiente
-        ArrayList<RespuestaDeCliente> respuestas1 = new ArrayList<RespuestaDeCliente>();
-        ArrayList<RespuestaDeCliente> respuestas2 = new ArrayList<RespuestaDeCliente>();
-        ArrayList<RespuestaDeCliente> respuestas3 = new ArrayList<RespuestaDeCliente>();
-        ArrayList<RespuestaDeCliente> respuestas4 = new ArrayList<RespuestaDeCliente>();
-        ArrayList<RespuestaDeCliente> respuestas5 = new ArrayList<RespuestaDeCliente>();
-
-        //Creacion objetos clientes
-        Cliente cliente1 = new Cliente("Roger Federer", 20357987, "3534078294"); //Enc1
-        Cliente cliente2 = new Cliente("Novak Djokovic", 22567987, "3534678341"); //Enc1
-        Cliente cliente3 = new Cliente("Rafael Nadal", 22345876, "3534079234"); //Enc2
-        Cliente cliente4 = new Cliente("Carlos Alcaraz", 40398523, "3538908213"); //Enc2
-        Cliente cliente5 = new Cliente("Maria Sharapova", 27997312, "3536098123"); //Enc1
-
-        //Fecha para las respuestas del cliente
-        Date fecha1 = new Date("06/17/2023"); //enc1
-        Date fecha2 = new Date("04/23/2023"); //enc1
-        Date fecha3 = new Date("07/25/2023"); //enc2
-        Date fecha4 = new Date("09/05/2023"); //enc2
-        Date fecha5 = new Date("01/02/2023"); //enc1
-
-        //Creacion de objetos preguntas y su agregacion al vector correspondiente
-        Pregunta pregunta1 = new Pregunta("¿Como calificaria el servicio?", vectorRespuestasCalificacion);
-        Pregunta pregunta2 = new Pregunta("¿Estas conforme con el servicio?", vectorRespuestasSiNo);
-        Pregunta pregunta3 = new Pregunta("¿Le recomendaria el servicio a un amigo?", vectorRespuestasSiNo);
-        Pregunta pregunta4 = new Pregunta("¿Como fue la calidad de la llamada?", vectorRespuestasCalificacion);
-
-        encuestas.removeAll(encuestas);
-        //Encuesta1
-        Date fechaInicioVigencia = new Date("01/01/2023");
-        Date fechaFinVigencia = new Date("06/30/2023");
-        Encuesta encuesta1 = new Encuesta(preguntas, "encuesta1", fechaInicioVigencia, fechaFinVigencia);
-        encuestas.add(encuesta1);
-
-        preguntas.removeAll(preguntas);
-        preguntas.add(pregunta1);
-        preguntas.add(pregunta2);
-        preguntas.add(pregunta3);
-
-        //Encuesta2
-        Date fechaInicioVigencia2 = new Date("07/01/2023");
-        Date fechaFinVigencia2 = new Date("12/31/2023");
-        Encuesta encuesta2 = new Encuesta(preguntas2, "encuesta2", fechaInicioVigencia2, fechaFinVigencia2);
-        encuestas.add(encuesta2);
-
-        preguntas2.removeAll(preguntas2);
-        preguntas2.add(pregunta2);
-        preguntas2.add(pregunta3);
-        preguntas2.add(pregunta4);
-
-        //Creacion objetos estados
-        Estado iniciada = new Estado("Iniciada");
-        Estado finalizada = new Estado("Finalizada");
-
-        //Creacion respuestas posibles y agregacion al vector correspondiente
-        RespuestaPosible rtaPosible1 = new RespuestaPosible("Si", 1);
-        RespuestaPosible rtaPosible2 = new RespuestaPosible("No", 0);
-        vectorRespuestasSiNo.add(rtaPosible1);
-        vectorRespuestasSiNo.add(rtaPosible2);
-
-        //Segundo array con respuestas posibles
-        RespuestaPosible rtaPosibleC1 = new RespuestaPosible("Malo", 1);
-        RespuestaPosible rtaPosibleC2 = new RespuestaPosible("Decente", 2);
-        RespuestaPosible rtaPosibleC3 = new RespuestaPosible("Bueno", 3);
-        RespuestaPosible rtaPosibleC4 = new RespuestaPosible("Excelente", 4);
-
-        vectorRespuestasCalificacion.add(rtaPosibleC1);
-        vectorRespuestasCalificacion.add(rtaPosibleC2);
-        vectorRespuestasCalificacion.add(rtaPosibleC3);
-        vectorRespuestasCalificacion.add(rtaPosibleC4);
-
-        //Respuestas del cliente 1 //Enc1
-        RespuestaDeCliente respuesta11 = new RespuestaDeCliente(rtaPosibleC2, fecha1);
-        RespuestaDeCliente respuesta12 = new RespuestaDeCliente(rtaPosible2, fecha1);
-        RespuestaDeCliente respuesta13 = new RespuestaDeCliente(rtaPosible1, fecha1);
-
-        respuestas1.add(respuesta11);
-        respuestas1.add(respuesta12);
-        respuestas1.add(respuesta13);
-
-        //Respuestas del cliente 2 //Enc1
-        RespuestaDeCliente respuesta21 = new RespuestaDeCliente(rtaPosibleC4, fecha2);
-        RespuestaDeCliente respuesta22 = new RespuestaDeCliente(rtaPosible1, fecha2);
-        RespuestaDeCliente respuesta23 = new RespuestaDeCliente(rtaPosible1, fecha2);
-        respuestas2.add(respuesta21);
-        respuestas2.add(respuesta22);
-        respuestas2.add(respuesta23);
-
-        //Respuestas del cliente 5 //Enc1
-        RespuestaDeCliente respuesta51 = new RespuestaDeCliente(rtaPosibleC1, fecha5);
-        RespuestaDeCliente respuesta52 = new RespuestaDeCliente(rtaPosible1, fecha5);
-        RespuestaDeCliente respuesta53 = new RespuestaDeCliente(rtaPosible2, fecha5);
-        respuestas5.add(respuesta51);
-        respuestas5.add(respuesta52);
-        respuestas5.add(respuesta53);
-
-        //Respuestas del cliente 3 //Enc2
-        RespuestaDeCliente respuesta31 = new RespuestaDeCliente(rtaPosible1, fecha3);
-        RespuestaDeCliente respuesta32 = new RespuestaDeCliente(rtaPosible1, fecha3);
-        RespuestaDeCliente respuesta33 = new RespuestaDeCliente(rtaPosibleC3, fecha3);
-        respuestas3.add(respuesta31);
-        respuestas3.add(respuesta32);
-        respuestas3.add(respuesta33);
-
-        //Respuestas del cliente 4 //Enc2
-        RespuestaDeCliente respuesta41 = new RespuestaDeCliente(rtaPosible2, fecha4);
-        RespuestaDeCliente respuesta42 = new RespuestaDeCliente(rtaPosible2, fecha4);
-        RespuestaDeCliente respuesta43 = new RespuestaDeCliente(rtaPosibleC1, fecha4);
-        respuestas4.add(respuesta41);
-        respuestas4.add(respuesta42);
-        respuestas4.add(respuesta43);
-
-        //Creacion objetos llamadas
-        Llamada llamada1 = new Llamada(1, cliente1, finalizada, respuestas1, "Prueba1", 10, true);
-        Llamada llamada2 = new Llamada(2, cliente2, iniciada, null, "Prueba 2", 0, false);
-        Llamada llamada3 = new Llamada(3, cliente2, finalizada, respuestas2, "Prueba 3", 9, true);
-        Llamada llamada4 = new Llamada(4, cliente3, finalizada, respuestas3, "Prueba 4", 7, true);
-        Llamada llamada5 = new Llamada(5, cliente4, finalizada, respuestas4, "Prueba 5", 14, true);
-        Llamada llamada6 = new Llamada(6, cliente5, finalizada, respuestas5, "Prueba 6", 5, true);
         
         
-        llamadas.add(llamada1);
-        llamadas.add(llamada2);
-        llamadas.add(llamada3);
-        llamadas.add(llamada4);
-        llamadas.add(llamada5);
-        llamadas.add(llamada6);
-
+        
+        
     }
 
 }
--- script_ppai.sql
+++ script_ppai.sql
@@ -52,6 +52,7 @@ CREATE Table Llamada (
 
 CREATE Table RespuestaDeCliente (
     id SERIAL, 
+    ordenRespuesta INT,
     fechaEncuesta DATE, 
     respuestaPosible INT, 
     llamada INT,
@@ -95,45 +96,43 @@ CREATE Table EncuestaAPregunta (
 );
 
 
-/* Datos para la tabla Estado */
+/*
 INSERT INTO Estado (nombre) VALUES
 ('Activo'),
 ('Inactivo'),
 ('En espera');
 
-/* Datos para la tabla Cliente */ 
+
 INSERT INTO Cliente (nombreCompleto, nroCelular,dni) VALUES
 ('Salera, Roberto', '123456789',43604050),
 ('Antunez, Jeremias', '987654321',43604051),
 ('Goti, Franco', '555666777',43604052);
 
-/* Datos para la tabla RespuestaPosible */
 INSERT INTO RespuestaPosible (descripcion, valor) VALUES
 ('Sí', 1),
 ('No', 0),
 ('Neutral', 2);
 
-/* Datos para la tabla Llamada */
+
 INSERT INTO Llamada (descripcionOperador, detalleAccionRequerida, duracion, encuestaEnviada, numeroLlamada, estado, cliente, observacionAuditor) VALUES
 ('Consulta de servicio', 'Ninguna', 120, true, 7, 1, 1, 'Todo en orden');
 
-/* Datos para la tabla RespuestaDeCliente */
+
 INSERT INTO RespuestaDeCliente (fechaEncuesta, respuestaPosible, llamada) VALUES
 ('11-25-2022', 1, 7),
 ('11-26-2022', 2, 7),
 ('11-27-2022', 3, 7);
 
-/* Datos para la tabla Pregunta */
+
 INSERT INTO Pregunta (descripcionPregunta) VALUES
 ('Esta satisfecho con nuestro servicio?'),
 ('Hay algo que podamos mejorar?');
 
-/* Datos para la tabla Encuesta */
+
 INSERT INTO Encuesta (descripcion, fechaInicioVigencia, fechaFinVigencia) VALUES
 ('Encuesta de satisfacción', '11-1-2022', '11-30-2022');
 
-/* Datos para las tablas intermedias */
-/* PreguntaARespuestaPosible */
+
 INSERT INTO PreguntaARespuestaPosible (respuestaPosible, pregunta) VALUES
 (1, 1),
 (2, 1),
@@ -141,10 +140,92 @@ INSERT INTO PreguntaARespuestaPosible (respuestaPosible, pregunta) VALUES
 (2, 2),
 (3, 2);
 
-/* EncuestaAPregunta */ 
 INSERT INTO EncuestaAPregunta (encuesta, pregunta) VALUES
 (1, 1),
 (1, 2);
+*/
 
+INSERT INTO Estado (nombre) VALUES
+('Iniciada'),
+('Finalizada');
 
+/* Datos para la tabla Cliente */
+INSERT INTO Cliente (nombreCompleto, dni, nroCelular) VALUES
+('Roger Federer', 20357987, '3534078294'),
+('Novak Djokovic', 22567987, '3534678341'),
+('Rafael Nadal', 22345876, '3534079234'),
+('Carlos Alcaraz', 40398523, '3538908213'),
+('Maria Sharapova', 27997312, '3536098123');
 
+/* Datos para la tabla RespuestaPosible */
+INSERT INTO RespuestaPosible (descripcion, valor) VALUES
+('Si', 0),
+('No', 1),
+('Malo', 2),
+('Decente', 3),
+('Bueno', 4),
+('Excelente', 5);
+
+/* Datos para la tabla Llamada */
+INSERT INTO Llamada (numeroLlamada, descripcionOperador, detalleAccionRequerida, duracion, encuestaEnviada, estado, cliente) VALUES
+(1, 'Prueba1', 'Prueba1', 10, true, 2, 1),
+(2, 'Prueba 2', 'Prueba 2', 0, false, 1, 2),
+(3, 'Prueba 3', 'Prueba 3', 9, true, 2, 2),
+(4, 'Prueba 4', 'Prueba 4', 7, true, 2, 3),
+(5, 'Prueba 5', 'Prueba 5', 14, true, 2, 4),
+(6, 'Prueba 6', 'Prueba 6', 5, true, 2, 5);
+
+/* Datos para la tabla RespuestaDeCliente */
+INSERT INTO RespuestaDeCliente (fechaEncuesta, respuestaPosible, llamada,ordenRespuesta) VALUES
+('06-17-2023',4,1,1),
+('06-17-2023',2,1,2),
+('06-17-2023',1,1,3),
+('04-23-2023',6,3,1),
+('04-23-2023',1,3,2),
+('04-23-2023',2,3,3),
+('07-25-2023',1,4,1),
+('07-25-2023',1,4,2),
+('07-25-2023',5,4,3),
+('09-05-2023',2,5,1),
+('09-05-2023',2,5,2),
+('09-05-2023',3,5,3),
+('01-02-2023',3,6,1),
+('01-02-2023',1,6,2),
+('01-02-2023',2,6,3);
+
+/* Datos para la tabla Pregunta */
+INSERT INTO Pregunta (descripcionPregunta) VALUES
+('¿Como calificaria el servicio?'),
+('¿Estas conforme con el servicio?'),
+('¿Le recomendaria el servicio a un amigo?'),
+('¿Como fue la calidad de la llamada?');
+
+/* Datos para la tabla Encuesta */
+INSERT INTO Encuesta (descripcion, fechaInicioVigencia, fechaFinVigencia) VALUES
+('encuesta1', '01-01-2023', '06-30-2023'),
+('encuesta2', '07-01-2023', '12-31-2023');
+
+/* Datos para las tablas intermedias */
+/* PreguntaARespuestaPosible */
+INSERT INTO PreguntaARespuestaPosible (respuestaPosible, pregunta) VALUES
+(3, 1),
+(4, 1),
+(5, 1),
+(6, 1),
+(1, 2),
+(2, 2),
+(1, 3),
+(2, 3),
+(3, 4),
+(4, 4),
+(5, 4),
+(6, 4);
+
+/* EncuestaAPregunta */
+INSERT INTO EncuestaAPregunta (encuesta, pregunta) VALUES
+(1,1),
+(1,2),
+(1,3),
+(2,2),
+(2,3),
+(2,4);
